e97e5e9ded6d545fcb5a4a1a0448d7d77e5531bb
Oksana77777 <cassandra.bk@mail.ru>
2023-05-09 21:21:27 +0300

    first commit

---
+++ src/test/java/FinanceManagerTest.java
@@ -0,0 +1,55 @@
+import org.example.FinanceManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+class FinanceManagerTest {
+    public final String food = "еда";
+    public final String other = "другое";
+
+    public Object[] valuesBuy = new Object[]{food, "2022.08.02", 200L};
+    public FinanceManager financeManager = new FinanceManager();
+
+    @Test
+    public void getMaxOfMapTest() {
+        Map<String, Long> subMap = Map.of(food, 200L, other, 300L);
+        Long expected = 300L;
+        Long actual = financeManager.getMaxOfMap(subMap).get(other);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxCategoryOfPeriodTest() {
+        Map<String, Map<String, Long>> spendCategoryOfPeriod = Map.of(
+                food, Map.of("2022", 200L),
+                other, Map.of("2022", 300L)
+        );
+        String period = "2022";
+        Long expected = 300L;
+        Long actual = financeManager.maxCategoryOfPeriod(spendCategoryOfPeriod, period).get(other);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void addBuyTest() {
+        Long expectedSum = 200L;
+
+        financeManager.addBuy(valuesBuy);
+        Long actualSum = financeManager.getSpendEntirePeriod().get(food);
+
+        Assertions.assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void maxCategoryTest() {
+        int expectedSize = 4;
+
+        financeManager.addBuy(valuesBuy);
+        int actualSize = financeManager.maxCategory().size();
+
+        Assertions.assertEquals(expectedSize, actualSize);
+    }
+}
